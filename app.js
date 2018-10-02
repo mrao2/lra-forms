026c906a0e68c499945df3f421c0cfdb3ff916f4
Mohith Rao <mohith.rao96@gmail.com>
2018-10-02 01:18:23 -0500

    stable

--- app.js
+++ app.js
@@ -22,44 +22,48 @@ app.get('/', (req,res) => {
 });
 
 app.post('/send', (req, res) => {
-    //console.log(req.body);
-    const output = `
-    <p>New Application!</p>
-    <h3>Form Details</h3>
-    <ul>
-        <li>Name: ${req.body.name}</li>
-        <li>Email: ${req.body.email}</li>
-    </ul>
-    `;
+    console.log(req.body);
+//     const output = `
+//     <p>New Application!</p>
+//     <h3>Form Details</h3>
+//     <ul>
+//         <li>Name: ${req.body.name}</li>
+//         <li>Email: ${req.body.email}</li>
+//     </ul>
+//     `;
 
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.stlvacancy.com',
-        port: "portname",
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass // generated ethereal password
-        }
-    });
+//     let transporter = nodemailer.createTransport({
+//         host: 'smtp.stlvacancy.com',
+//         port: "portname",
+//         secure: false, // true for 465, false for other ports
+//         auth: {
+//             user: account.user, // generated ethereal user
+//             pass: account.pass // generated ethereal password
+//         },
+//         //OPTIONAL
+//         // tls:{
+//         //     rejectUnauthorized: false
+//         // }
+//     });
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"StlVacancy" <app@stlvacancy.com>', // sender address
-        to: 'test@lra.com', // list of receivers
-        subject: 'New Application', // Subject line
-        text: 'Hello world?', // plain text body
-        html: output // html body
-    };
+//     // setup email data with unicode symbols
+//     let mailOptions = {
+//         from: '"StlVacancy" <app@stlvacancy.com>', // sender address
+//         to: 'test@lra.com', // list of receivers
+//         subject: 'New Application', // Subject line
+//         text: 'Hello world?', // plain text body
+//         html: output // html body
+//     };
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+//     // send mail with defined transport object
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             return console.log(error);
+//         }
+//         console.log('Message sent: %s', info.messageId);
+//         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-        res.render('contact', {msg:"Your application has been sent!"})
-});
+//         res.render('contact', {msg:"Your application has been sent!"})
+ });
 
 app.listen(8000, () => console.log("Server started...."));
